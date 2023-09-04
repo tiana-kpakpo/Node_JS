8b1f9abc948fb398106522a9b0f129c9d1207e4f
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2023-09-04 11:39:43 -0700

    update

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const routes = require('./src/routes/routes')
+const path = require('path')
+
+
+//create new instance of express
+const app = express();
+const PORT = 5151;  //server port
+
+
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors({
+    origin : '*'
+}))
+//add public folder to the client 
+app.use(express.static(path.join(__dirname, './src/public')));
+
+//use routes
+// app.use('/', routes)
+
+//default application root
+app.get('/', (req, res) => {
+    res.sendFile('index.html', err => {
+        if (err) {
+          res.status(403).send('error index.html not found');
+        }
+      });
+})
+
+
+//handle bad url requests
+app.use((req, res) => {
+    res.sendFile('notFound.html', err => {
+        if (err) {
+          res.status(500).send('internal server error');
+        }
+      });
+  });
+
+
+  //start app on this port
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
