0ec8ef78953f887d472e9bd1e2ba739fcdf4f371
tiana-kpakpo <tianakpakpo1@gmail.com>
2023-09-05 10:05:42 +0000

    first

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -2,7 +2,7 @@
 const users = {
     user1: {
         name: 'Tiana',
-        email: 'tianakpakpo@21',
+        email: 'tianakpakpo@gmail.com',
         password: 'Glory@21'
     }
 }
@@ -14,31 +14,15 @@ const loginUser = async (req, res) => {
 
    
     const user = users[email];
+    
 
     if(!user){
         res.status(404).json({message: 'User not found'})
         return;
     }
 
-
-    // if (err) {
-    //     console.error('Password comparison error:', err);
-    //     res.status(500).json({ message: 'Internal server error' });
-    //   } else if (result) {
-    //     // Passwords match, user is authenticated
-    //     res.status(200).json({
-    //       name: user.name,
-    //       email: user.email,
-    //       message: 'Login successful',
-    //     });
-    //   } else {
-    //     // Passwords don't match, deny login
-    //     res.status(401).json({ message: 'Wrong password' });
-    //   }
-
-
    //simulate database
-   if(email != "tianakpakpo@21"){
+   if(email != "tianakpakpo@gmail.com"){
     res.status(303).json({message: "user not found"})
 
    }else{
@@ -46,23 +30,23 @@ const loginUser = async (req, res) => {
             {
                 name: "Tiana",
                 password: "req.body.password",
-                email: "email"
+                email: "tianakpakpo@gmail.com"
             }
         )
    }
-   
 
-//    if(password != "Glory@21"){
-//     res.status(303).json({message: "wrong password"})
-//    }else{
-//     res.status(200).json(
-//             {
-//                 name: "Tiana",
-//                 password: "req.body.password",
-//                 email: "email"
-//             }
-//         )
-//    }
+
+   if(password != "Glory@21"){
+    res.status(303).json({message: "wrong password"})
+   }else{
+    res.status(200).json(
+            {
+                name: "Tiana",
+                password: "req.body.password",
+                email: "tianakpakpo@gmail.com"
+            }
+        )
+   }
 
 
 
